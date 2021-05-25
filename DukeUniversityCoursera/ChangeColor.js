98fbc7f13388f0f0a9149e9c46e3e85d3fb86e2f
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-25 11:31:27 +0600

    conncet html with both CSS and JavaScript for the very first time Alhamdulillah

---
+++ DukeUniversityCoursera/ChangeColor.js
@@ -0,0 +1,15 @@
+function doLime(){
+    var lime = document.getElementById("canvas1");
+    lime.style.backgroundColor="lime";
+}
+
+function doAqua(){
+    var aqua = document.getElementById("canvas1");
+    aqua.style.backgroundColor="aqua";
+}
+
+
+function doOrange(){
+    var orange = document.getElementById("canvas1");
+    orange.style.backgroundColor="orange";
+}
