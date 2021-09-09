f7aa1abf96fea31c2604ddec7ef44f51e5f4afc5
rakib3004 <bsse1129@iit.du.ac.bd>
2021-09-09 23:06:23 +0600

    My first sucessfully runable javasript program

---
+++ 5Miniute/BigNotationO.js
@@ -0,0 +1,12 @@
+const teamRed = ["Galib","Mushfiq","Muktar","Mohsin","Mustahid","Kazi"]
+const temaBlue = ["Jion","Diganta","Mahin","Shuvo","Shahed","Robin"]
+
+const big_o_notation = (fromTeamRed, fromTeamBlue) =>{
+    let teamRedMember= fromTeamRed[2]
+    console.log(teamRedMember)
+    let teamBlueMember= fromTeamBlue[2]
+    console.log(teamBlueMember)
+}
+
+big_o_notation(teamRed,temaBlue)
+
