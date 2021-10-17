23bf26ce824ed464cfb4ec1aae0fc353b8fa97ce
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-17 10:27:34 +0600

    debug javascript code

--- 5Miniute/BigNotationO.js
+++ 5Miniute/BigNotationO.js
@@ -1,12 +1,12 @@
-const teamRed = ["Galib","Mushfiq","Muktar","Mohsin","Mustahid","Kazi"]
-const temaBlue = ["Jion","Diganta","Mahin","Shuvo","Shahed","Robin"]
+const teamRed = ["Galib","Mushfiq","Muktar","Mohsin","Mustahid","Kazi"];
+const temaBlue = ["Jion","Diganta","Mahin","Shuvo","Shahed","Robin"];
 
 const big_o_notation = (fromTeamRed, fromTeamBlue) =>{
-    let teamRedMember= fromTeamRed[2]
-    console.log(teamRedMember)
-    let teamBlueMember= fromTeamBlue[2]
-    console.log(teamBlueMember)
+    let teamRedMember= fromTeamRed[2];
+    console.log(teamRedMember);
+    let teamBlueMember= fromTeamBlue[2];
+    console.log(teamBlueMember);
 }
 
-big_o_notation(teamRed,temaBlue)
+big_o_notation(teamRed,temaBlue);
 
